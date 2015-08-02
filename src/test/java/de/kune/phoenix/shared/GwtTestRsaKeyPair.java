b397022259b1d7cb778acb7b688f3d3ce9d55b46
Alexander Kune <alexander@kune.de>
2015-08-02 13:47:12 +0200

    Added test cases for private key encryption and public key decryption.

---
+++ src/test/java/de/kune/phoenix/shared/GwtTestRsaKeyPair.java
@@ -0,0 +1,137 @@
+package de.kune.phoenix.shared;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+import com.google.gwt.core.shared.GWT;
+
+import de.kune.phoenix.shared.RsaKeyPair.KeyType;
+import de.kune.phoenix.shared.RsaKeyPair.MessageFormat;
+
+public class GwtTestRsaKeyPair extends AsyncGwtTestBase {
+
+	private static final String PRIVATE_KEY = "4wxbdmWP5JYXXdxxfq3fqHa8BunOHCGib0m5dwRkar1J8xajgNeVTVUnZdPzpuAX6xxPUVlbcXLRBuhpHWxaE2bPIs5ZI0tQxxsaRIgukrXEpmw4gPCVmzNSkwfopBm8xx2gCZ3pRDnMEvfXhxbJTQ5xxgxxyfkQ";
+	private static final String PUBLIC_KEY = "cbVz3B4xbxx2E3l4l0LeCnjD7GkFXyshtxxhtnN5hF8sUVjmdD0zYAQgO7ca2pN4pazU5R7xx1EGr80jZBW5WZH0yA";
+
+	@Override
+	public String getModuleName() {
+		return "de.kune.phoenix.mainjunit";
+	}
+
+	public void testEncryptPublicDecryptPrivateWithGivenKeyPairBitPadding() {
+		delayTestFinish(120000);
+		RsaKeyPairGenerator generator = new RsaKeyPairGenerator();
+		generator.createAsync(PUBLIC_KEY, PRIVATE_KEY, new TestCallback<RsaKeyPair, Exception>() {
+			@Override
+			public void handleSuccess(RsaKeyPair result) {
+				try {
+					result.setMessageFormat(MessageFormat.BitPadding);
+					assertNotNull(result);
+					try {
+						final String plainText = "Plain Text";
+						GWT.log("plain text: " + plainText);
+						byte[] plainBytes = plainText.getBytes("UTF-8");
+						GWT.log("plain bytes: " + Arrays.toString(plainBytes));
+						byte[] encrypted = result.encrypt(KeyType.PUBLIC, plainBytes);
+						GWT.log("Java encrypted: " + Arrays.toString(encrypted));
+						byte[] decryptedBytes = result.decrypt(KeyType.PRIVATE, encrypted);
+						GWT.log("decrypted bytes: " + decryptedBytes);
+						assertEquals(plainText, new String(decryptedBytes, "UTF-8"));
+					} catch (UnsupportedEncodingException e) {
+						fail("UTF-8 not supported");
+					}
+				} catch (Throwable e) {
+					reportUncaughtException(e);
+				}
+			}
+		});
+	}
+
+	public void testEncryptPublicDecryptPrivateWithGivenKeyPairSoaepPadding() {
+		delayTestFinish(120000);
+		RsaKeyPairGenerator generator = new RsaKeyPairGenerator();
+		generator.createAsync(PUBLIC_KEY, PRIVATE_KEY, new TestCallback<RsaKeyPair, Exception>() {
+			@Override
+			public void handleSuccess(RsaKeyPair result) {
+				try {
+					result.setMessageFormat(MessageFormat.SOAEP);
+					assertNotNull(result);
+					try {
+						final String plainText = "Plain Text";
+						GWT.log("plain text: " + plainText);
+						byte[] plainBytes = plainText.getBytes("UTF-8");
+						GWT.log("plain bytes: " + Arrays.toString(plainBytes));
+						byte[] encrypted = result.encrypt(KeyType.PUBLIC, plainBytes);
+						GWT.log("Java encrypted: " + Arrays.toString(encrypted));
+						byte[] decryptedBytes = result.decrypt(KeyType.PRIVATE, encrypted);
+						GWT.log("decrypted bytes: " + decryptedBytes);
+						assertEquals(plainText, new String(decryptedBytes, "UTF-8"));
+					} catch (UnsupportedEncodingException e) {
+						fail("UTF-8 not supported");
+					}
+				} catch (Throwable e) {
+					reportUncaughtException(e);
+				}
+			}
+		});
+	}
+	
+	public void testEncryptPrivateDecryptPublicWithGivenKeyPairSoaepPadding() {
+		delayTestFinish(120000);
+		RsaKeyPairGenerator generator = new RsaKeyPairGenerator();
+		generator.createAsync(PUBLIC_KEY, PRIVATE_KEY, new TestCallback<RsaKeyPair, Exception>() {
+			@Override
+			public void handleSuccess(RsaKeyPair result) {
+				try {
+					result.setMessageFormat(MessageFormat.SOAEP);
+					assertNotNull(result);
+					try {
+						final String plainText = "Plain Text";
+						GWT.log("plain text: " + plainText);
+						byte[] plainBytes = plainText.getBytes("UTF-8");
+						GWT.log("plain bytes: " + Arrays.toString(plainBytes));
+						byte[] encrypted = result.encrypt(KeyType.PRIVATE, plainBytes);
+						GWT.log("Java encrypted: " + Arrays.toString(encrypted));
+						byte[] decryptedBytes = result.decrypt(KeyType.PUBLIC, encrypted);
+						GWT.log("decrypted bytes: " + decryptedBytes);
+						assertEquals(plainText, new String(decryptedBytes, "UTF-8"));
+					} catch (UnsupportedEncodingException e) {
+						fail("UTF-8 not supported");
+					}
+				} catch (Throwable e) {
+					reportUncaughtException(e);
+				}
+			}
+		});
+	}
+	
+	public void testEncryptPrivateDecryptPublicWithGivenKeyPairBitPadding() {
+		delayTestFinish(120000);
+		RsaKeyPairGenerator generator = new RsaKeyPairGenerator();
+		generator.createAsync(PUBLIC_KEY, PRIVATE_KEY, new TestCallback<RsaKeyPair, Exception>() {
+			@Override
+			public void handleSuccess(RsaKeyPair result) {
+				try {
+					result.setMessageFormat(MessageFormat.BitPadding);
+					assertNotNull(result);
+					try {
+						final String plainText = "Plain Text";
+						GWT.log("plain text: " + plainText);
+						byte[] plainBytes = plainText.getBytes("UTF-8");
+						GWT.log("plain bytes: " + Arrays.toString(plainBytes));
+						byte[] encrypted = result.encrypt(KeyType.PRIVATE, plainBytes);
+						GWT.log("Java encrypted: " + Arrays.toString(encrypted));
+						byte[] decryptedBytes = result.decrypt(KeyType.PUBLIC, encrypted);
+						GWT.log("decrypted bytes: " + decryptedBytes);
+						assertEquals(plainText, new String(decryptedBytes, "UTF-8"));
+					} catch (UnsupportedEncodingException e) {
+						fail("UTF-8 not supported");
+					}
+				} catch (Throwable e) {
+					reportUncaughtException(e);
+				}
+			}
+		});
+	}
+	
+}
--- src/test/java/de/kune/phoenix/shared/GwtTestRsaKeyPairGenerator.java
+++ src/test/java/de/kune/phoenix/shared/GwtTestRsaKeyPairGenerator.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import com.google.gwt.core.shared.GWT;
 
 import de.kune.phoenix.shared.RsaKeyPair.KeyType;
-import de.kune.phoenix.shared.RsaKeyPair.MessageFormat;
 import de.kune.phoenix.shared.RsaKeyPairGenerator.KeyStrength;
 import de.kune.phoenix.shared.RsaKeyPairGenerator.PublicExponent;
 
@@ -15,38 +14,6 @@ public class GwtTestRsaKeyPairGenerator extends AsyncGwtTestBase {
 	@Override
 	public String getModuleName() {
 		return "de.kune.phoenix.mainjunit";
-	}
-
-	public void testEncryptPublicDecryptPrivateWithGivenKeyPair() {
-		delayTestFinish(120000);
-		RsaKeyPairGenerator generator = new RsaKeyPairGenerator();
-		generator.createAsync(
-				"cbVz3B4xbxx2E3l4l0LeCnjD7GkFXyshtxxhtnN5hF8sUVjmdD0zYAQgO7ca2pN4pazU5R7xx1EGr80jZBW5WZH0yA",
-				"4wxbdmWP5JYXXdxxfq3fqHa8BunOHCGib0m5dwRkar1J8xajgNeVTVUnZdPzpuAX6xxPUVlbcXLRBuhpHWxaE2bPIs5ZI0tQxxsaRIgukrXEpmw4gPCVmzNSkwfopBm8xx2gCZ3pRDnMEvfXhxbJTQ5xxgxxyfkQ",
-				new TestCallback<RsaKeyPair, Exception>() {
-					@Override
-					public void handleSuccess(RsaKeyPair result) {
-						try {
-							result.setMessageFormat(MessageFormat.BitPadding);
-							assertNotNull(result);
-							try {
-								final String plainText = "Plain Text";
-								GWT.log("plain text: " + plainText);
-								byte[] plainBytes = plainText.getBytes("UTF-8");
-								GWT.log("plain bytes: " + Arrays.toString(plainBytes));
-								byte[] encrypted = result.encrypt(KeyType.PUBLIC, plainBytes);
-								GWT.log("Java encrypted: " + Arrays.toString(encrypted));
-								byte[] decryptedBytes = result.decrypt(KeyType.PRIVATE, encrypted);
-								GWT.log("decrypted bytes: " + decryptedBytes);
-								assertEquals(plainText, new String(decryptedBytes, "UTF-8"));
-							} catch (UnsupportedEncodingException e) {
-								fail("UTF-8 not supported");
-							}
-						} catch (Throwable e) {
-							reportUncaughtException(e);
-						}
-					}
-				});
 	}
 
 	public void testEncryptPublicDecryptPrivateWithGeneratedKey() {
