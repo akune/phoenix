6bda2bab8d7d0a63996a9d07e6c2c4b0f8f6f7f7
Alexander Kune <alexander@kune.de>
2015-08-02 09:43:00 +0200

    Fixed tests.

--- src/test/java/de/kune/phoenix/shared/GwtTestRsaKeyPairGenerator.java
+++ src/test/java/de/kune/phoenix/shared/GwtTestRsaKeyPairGenerator.java
@@ -18,41 +18,46 @@ public class GwtTestRsaKeyPairGenerator extends GWTTestCase {
 	public String getModuleName() {
 		return "de.kune.phoenix.mainjunit";
 	}
-	
+
 	public void testEncryptPublicDecryptPrivateWithGivenKeyPair() {
-		RsaKeyPairGenerator generator = new RsaKeyPairGenerator();
-		generator.createAsync("cbVz3B4xbxx2E3l4l0LeCnjD7GkFXyshtxxhtnN5hF8sUVjmdD0zYAQgO7ca2pN4pazU5R7xx1EGr80jZBW5WZH0yA", "4wxbdmWP5JYXXdxxfq3fqHa8BunOHCGib0m5dwRkar1J8xajgNeVTVUnZdPzpuAX6xxPUVlbcXLRBuhpHWxaE2bPIs5ZI0tQxxsaRIgukrXEpmw4gPCVmzNSkwfopBm8xx2gCZ3pRDnMEvfXhxbJTQ5xxgxxyfkQ", new Callback<RsaKeyPair, Exception>() {
-			@Override
-			public void onSuccess(RsaKeyPair result) {
-				try {
-					result.setMessageFormat(MessageFormat.BitPadding);
-					assertNotNull(result);
-					try {
-						final String plainText = "Plain Text";
-						GWT.log("plain text: " + plainText);
-						byte[] plainBytes = plainText.getBytes("UTF-8");
-						GWT.log("plain bytes: " + Arrays.toString(plainBytes));
-						byte[] encrypted = result.encrypt(KeyType.PUBLIC, plainBytes);
-						GWT.log("Java encrypted: " + Arrays.toString(encrypted));
-						byte[] decryptedBytes = result.decrypt(KeyType.PRIVATE, encrypted);
-						GWT.log("decrypted bytes: " + decryptedBytes);
-						assertEquals(plainText,
-								new String(decryptedBytes, "UTF-8"));
-						finishTest();
-					} catch (UnsupportedEncodingException e) {
-						fail("UTF-8 not supported");
-					}
-				} catch (Throwable e) {
-					reportUncaughtException(e);
-				}
-			}
-			@Override
-			public void onFailure(Exception reason) {}
-		});
 		delayTestFinish(120000);
+		RsaKeyPairGenerator generator = new RsaKeyPairGenerator();
+		generator.createAsync(
+				"cbVz3B4xbxx2E3l4l0LeCnjD7GkFXyshtxxhtnN5hF8sUVjmdD0zYAQgO7ca2pN4pazU5R7xx1EGr80jZBW5WZH0yA",
+				"4wxbdmWP5JYXXdxxfq3fqHa8BunOHCGib0m5dwRkar1J8xajgNeVTVUnZdPzpuAX6xxPUVlbcXLRBuhpHWxaE2bPIs5ZI0tQxxsaRIgukrXEpmw4gPCVmzNSkwfopBm8xx2gCZ3pRDnMEvfXhxbJTQ5xxgxxyfkQ",
+				new Callback<RsaKeyPair, Exception>() {
+					@Override
+					public void onSuccess(RsaKeyPair result) {
+						try {
+							result.setMessageFormat(MessageFormat.BitPadding);
+							assertNotNull(result);
+							try {
+								final String plainText = "Plain Text";
+								GWT.log("plain text: " + plainText);
+								byte[] plainBytes = plainText.getBytes("UTF-8");
+								GWT.log("plain bytes: " + Arrays.toString(plainBytes));
+								byte[] encrypted = result.encrypt(KeyType.PUBLIC, plainBytes);
+								GWT.log("Java encrypted: " + Arrays.toString(encrypted));
+								byte[] decryptedBytes = result.decrypt(KeyType.PRIVATE, encrypted);
+								GWT.log("decrypted bytes: " + decryptedBytes);
+								assertEquals(plainText, new String(decryptedBytes, "UTF-8"));
+								finishTest();
+							} catch (UnsupportedEncodingException e) {
+								fail("UTF-8 not supported");
+							}
+						} catch (Throwable e) {
+							reportUncaughtException(e);
+						}
+					}
+
+					@Override
+					public void onFailure(Exception reason) {
+					}
+				});
 	}
 
-	public void testEncryptPublicDecryptPrivate() {
+	public void testEncryptPublicDecryptPrivateWithGeneratedKey() {
+		delayTestFinish(120000);
 		RsaKeyPairGenerator generator = new RsaKeyPairGenerator();
 		generator.generateKeyPairAsync(KeyStrength.WEAK, PublicExponent.SMALLEST,
 				new Callback<RsaKeyPair, Exception>() {
@@ -69,8 +74,7 @@ public class GwtTestRsaKeyPairGenerator extends GWTTestCase {
 								GWT.log("Java encrypted: " + Arrays.toString(encrypted));
 								byte[] decryptedBytes = result.decrypt(KeyType.PRIVATE, encrypted);
 								GWT.log("decrypted bytes: " + Arrays.toString(decryptedBytes));
-								assertEquals(plainText,
-										new String(decryptedBytes, "UTF-8"));
+								assertEquals(plainText, new String(decryptedBytes, "UTF-8"));
 								finishTest();
 							} catch (UnsupportedEncodingException e) {
 								fail("UTF-8 not supported");
@@ -102,10 +106,10 @@ public class GwtTestRsaKeyPairGenerator extends GWTTestCase {
 						}
 					}
 				});
-		delayTestFinish(120000);
 	}
 
 	public void testGenerateRsaKey() {
+		delayTestFinish(120000);
 		RsaKeyPairGenerator generator = new RsaKeyPairGenerator();
 		generator.generateKeyPairAsync(KeyStrength.WEAK, PublicExponent.SMALLEST,
 				new Callback<RsaKeyPair, Exception>() {
@@ -145,7 +149,6 @@ public class GwtTestRsaKeyPairGenerator extends GWTTestCase {
 						}
 					}
 				});
-		delayTestFinish(120000);
 	}
 
 }
