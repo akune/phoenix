73f8484add5bef9f9ab0b86e3925bc4ab0af98f4
Alexander Kune <alexander@kune.de>
2016-03-02 08:43:36 +0100

    Provide a method to receive a specific message by message id.

--- src/main/java/de/kune/phoenix/client/messaging/communication/RestMessageService.java
+++ src/main/java/de/kune/phoenix/client/messaging/communication/RestMessageService.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -27,5 +28,10 @@ public interface RestMessageService extends RestService {
 	@Produces(MediaType.APPLICATION_JSON)
 	void get(@QueryParam("wait") boolean wait, @QueryParam("last-sequence-key") String lastSequenceKey,
 			@QueryParam("recipient-id") String recipientId, MethodCallback<List<Message>> callback);
+	
+	@GET
+	@Path("message/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	void get(@PathParam("messageId") String messageId, MethodCallback<Message> callback);
 
 }
